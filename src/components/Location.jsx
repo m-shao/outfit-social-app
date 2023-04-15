5be4d3df08df29e3c8b71ec6798bfbd79d9689d1
r-chong <mubblybub@gmail.com>
2023-04-14 23:19:36 -0400

    api call working to openmeteo

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -1,8 +1,8 @@
-import {} from 'React';
 import { useState, useEffect } from 'react';
 
 export default function App() {
   const [location, setLocation] = useState(null);
+  const [weather, setWeather] = useState({});
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
@@ -10,6 +10,22 @@ export default function App() {
       setLocation({ latitude, longitude });
     });
   });
+
+  useEffect(() => {
+    fetch(
+      'https://api.open-meteo.com/v1/forecast?latitude=43.88&longitude=-78.93&hourly=temperature_2m,precipitation_probability,rain,showers,snowfall&current_weather=true',
+      {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+          Origin: 'http://localhost:5173',
+        },
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.error(error));
+  }, []);
 
   return (
     <div className="App">
@@ -20,6 +36,20 @@ export default function App() {
       ) : (
         <p>Retrieving location...</p>
       )}
+
+      <h1>
+        Weather in {weather.city_name}, {weather.country_code}
+      </h1>
+      <p>
+        Temperature: {weather.data && weather.data.temperature_2m[0].value}°C
+      </p>
+      <p>
+        Precipitation Probability:{' '}
+        {weather.data && weather.data.precipitation_probability[0].value}%
+      </p>
+      <p>Rain: {weather.data && weather.data.rain[0].value}mm</p>
+      <p>Showers: {weather.data && weather.data.showers[0].value}%</p>
+      <p>Snowfall: {weather.data && weather.data.snowfall[0].value}cm</p>
     </div>
   );
 }
