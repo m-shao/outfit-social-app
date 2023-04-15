fb9fabc5299edca6d34e17591a1112c678b0038c
r-chong <mubblybub@gmail.com>
2023-04-15 13:05:41 -0400

    weather api up and running

--- src/components/UserLocation.jsx
+++ src/components/UserLocation.jsx
@@ -18,7 +18,7 @@ export default function App() {
     if (location) {
       setLoading(true);
       fetch(
-        `https://api.open-meteo.com/v1/forecast?latitude=${location.latitude}&longitude=${location.longitude}&daily=weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset,uv_index_max,precipitation_probability_max,windspeed_10m_max&current_weather=true&forecast_days=1`,
+        `https://api.open-meteo.com/v1/forecast?latitude=${location.latitude}&longitude=${location.longitude}&daily=weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset,uv_index_max,precipitation_probability_max,windspeed_10m_max&current_weather=true&forecast_days=1&timezone=auto`,
         {
           method: 'GET',
           mode: 'cors',
@@ -36,60 +36,27 @@ export default function App() {
     }
   }, [location]);
 
-  useEffect(() => {
-    if (location) {
-      setLoading(true);
-      fetch(
-        `http://api.timezonedb.com/v2.1/get-time-zone?key=OW0FUQN3IH3H&format=xml&by=position&lat=${location.latitude}&lng=${location.longitude}`,
-        {
-          method: 'GET',
-          mode: 'cors',
-          headers: {
-            Origin: 'http://localhost:5173',
-          },
-        }
-      )
-        .then((response) => response.text())
-        .then((data) => {
-          const parsedData = parser.parse(data);
-          const timezoneInfo = parsedData && parsedData.timezone;
-          setTimezone(timezoneInfo);
-          setLoading(false);
-        })
-        .catch((error) => console.error(error));
-    }
-  }, [location]);
-
   return (
     <div className="App">
-      {location ? (
-        <p>
-          location is {location.longitude}, {location.latitude}
-        </p>
-      ) : (
-        <p>Retrieving location...</p>
-      )}
-
-      {loading ? (
-        <p>Loading weather data...</p>
-      ) : (
-        <>
-          <h1>
-            Weather in {weather.city_name}, {weather.country_code}
-          </h1>
+      <div className="App">
+        {location ? (
           <p>
-            Temperature: {weather.data && weather.data.temperature_2m[0].value}
-            °C
+            location is {location.longitude}, {location.latitude}
+            {weather && weather.current_weather && (
+              <div>
+                <p>Temperature: {weather.current_weather.temperature}</p>
+                <p>Wind Speed: {weather.current_weather.windspeed}</p>
+                <p>Wind Direction: {weather.current_weather.winddirection}</p>
+                <p>Weather Code: {weather.current_weather.weathercode}</p>
+                <p>Is Day: {weather.current_weather.is_day ? 'Yes' : 'No'}</p>
+                <p>Time: {weather.current_weather.time}</p>
+              </div>
+            )}
           </p>
-          <p>
-            Precipitation Probability:{' '}
-            {weather.data && weather.data.precipitation_probability[0].value}%
-          </p>
-          <p>Rain: {weather.data && weather.data.rain[0].value}mm</p>
-          <p>Showers: {weather.data && weather.data.showers[0].value}%</p>
-          <p>Snowfall: {weather.data && weather.data.snowfall[0].value}cm</p>
-        </>
-      )}
+        ) : (
+          <p>Retrieving location...</p>
+        )}
+      </div>
     </div>
   );
 }
