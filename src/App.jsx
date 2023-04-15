e1a575ea820e84b5bd098c14ba6d59b5b8dcec1c
m-shao <s201083816@ddsbstudent.ca>
2023-04-14 20:41:07 -0400

    added font

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import { useState } from 'react'
+import Navbar from './components/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      App
+      <Navbar/>
     </div>
   )
 }
