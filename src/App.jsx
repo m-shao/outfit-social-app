4e12246a0f3c399479698ac47a66377bae0799ca
m-shao <s201083816@ddsbstudent.ca>
2023-04-14 21:30:03 -0400

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
+import { useState } from 'react'
 import Navbar from './components/Navbar'
 
 function App() {
-
-  return (
-    <div className="App">
-      <Navbar/>
-    </div>
-  )
+    const [selectedNav, setSelectedNav] = useState('feed')
+    
+    return (
+        <div className="App">
+            <Navbar
+            selectedNav={selectedNav}
+            setSelectedNav={setSelectedNav}/>
+        </div>
+    )
 }
 
 export default App
