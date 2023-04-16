09ff3e7b4cb66073351c3778f70acfaeca6c0413
r-chong <mubblybub@gmail.com>
2023-04-16 00:29:05 -0400

    created basic profile page

--- src/routes/Profile.jsx
+++ src/routes/Profile.jsx
@@ -14,10 +14,50 @@ const Profile = () => {
     }
 
     return (
-        <div>
-            <p>Welcome, {user.name}!</p>
-            <p>Your email is: {user.email}</p>
-            <img src={user.picture} alt='Profile' />
+        <div className="w-full">
+            <div className="bg-white">
+                {/* Center content and pad */}
+                <div className="mx-auto px-4 sm:px-6 lg:px-8">
+                    <div className="py-6 md:flex md:items-center md:justify-between">
+                        <div className="flex-1 min-w-0">
+                            <div className="flex items-center">
+                                <img
+                                    // pfp
+                                    className="mx-10 hidden h-32 w-32 rounded-full sm:block"
+                                    src={user.picture}
+                                    alt="Profile"
+                                />
+                                <div>
+                                    <div className="flex items-center">
+                                        <img
+                                            className="h-16 w-16 rounded-full sm:hidden"
+                                            src={user.picture}
+                                            alt="Profile"
+                                        />
+                                        {/* username */}
+                                        <h1 className="ml-3 text-2xl font-bold leading-7 text-gray-900 sm:leading-9 sm:truncate">
+                                            {user.name}
+                                        </h1>
+                                    </div>
+                                    {/* email */}
+                                    <dl className="mt-6 flex flex-col sm:ml-3 sm:mt-1 sm:flex-row sm:flex-wrap">
+                                        <dt className="sr-only">Email</dt>
+                                        <dd className="flex items-center text-sm leading-5 text-gray-500 font-medium capitalize">
+                                            {user.email}
+                                        </dd>
+                                    </dl>
+                                    <dl className="border-4 bg-gray-200 mt-6 flex flex-col sm:ml-3 sm:mt-1 sm:flex-row sm:flex-wrap">
+                                        <dt className="sr-only">Bio</dt>
+                                        <dd className="flex items-center text-sm leading-5 text-gray-500 font-medium capitalize">
+                                            {user.bio}
+                                        </dd>
+                                    </dl>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <LogoutButton />
         </div>
     );
